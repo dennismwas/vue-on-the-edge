d44d3b558fdcb14be2c8c3e00e990934b14a5401
Dennoh <dennismwas89@gmail.com>
2023-11-02 00:49:45 +0300

    headers added

--- api/ssr.js
+++ api/ssr.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     console.log('Request to url:', url)
     if (url === undefined) throw new Error('req.url is undefined')
 
-    const pageContextInit = { urlOriginal: url }
+    const pageContextInit = { urlOriginal: url, headers: req, headers }
     const pageContext = await renderPage(pageContextInit)
     const { httpResponse } = pageContext
     console.log('httpResponse', !!httpResponse)
