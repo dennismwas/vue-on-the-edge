d8c49df3777ab26ed01cecaec1f81bcb64b63d6e
Dennoh <dennismwas89@gmail.com>
2023-11-19 15:25:26 +0300

    exclude vue from optimized deps

--- api/ssr.js
+++ api/ssr.js
@@ -4,13 +4,13 @@ export const config = {
     runtime: 'edge',
 };
 
-export default async function handler(req, res) {
-    const { url } = req
+export default async function handler(request) {
+    const { url } = request
     if (url === undefined) throw new Error('req.url is undefined')
 
 
     const dateString = new Date().toISOString();
-    const parsedCity = decodeURIComponent(req.headers['x-vercel-ip-city']);
+    const parsedCity = decodeURIComponent(request.headers['x-vercel-ip-city']);
     const ip = (req.headers['x-forwarded-for'] ?? '127.0.0.1').split(',')[0];
     const city = parsedCity == 'undefined' ? "Cannot get city" : parsedCity;
     
@@ -19,13 +19,13 @@ export default async function handler(req, res) {
     const { httpResponse } = pageContext
 
     if (!httpResponse) {
-        res.statusCode = 200
-        res.end()
+        Response.statusCode = 200
+        Response.end()
         return
     }
 
     const { body, statusCode, contentType } = httpResponse
-    res.statusCode = statusCode
-    res.setHeader('content-type', contentType)
-    res.end(body)
+    Response.statusCode = statusCode
+    Response.setHeader('content-type', contentType)
+    Response.end(body)
 }
