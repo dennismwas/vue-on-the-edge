1dc327785011d8c3cd48d94f8420e70e5375dce7
Dennoh <dennismwas89@gmail.com>
2023-11-19 15:55:14 +0300

    edge function configured

--- api/ssr.js
+++ api/ssr.js
@@ -5,25 +5,24 @@ export const config = {
 };
 
 export default async function handler(request) {
-    console.log(request.headers.get('x-forwarded-for'));
     const { url } = request
     if (url === undefined) throw new Error('req.url is undefined')
 
 
     const dateString = new Date().toISOString();
-    const parsedCity = decodeURIComponent(request.headers['x-vercel-ip-city']);
-    const ip = (request.headers['x-forwarded-for'] ?? '127.0.0.1').split(',')[0];
+    const parsedCity = decodeURIComponent(request.headers.get('x-vercel-ip-city'));
+    const ip = (request.headers.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0];
     const city = parsedCity == 'undefined' ? "Cannot get city" : parsedCity;
 
     const pageContextInit = { parsedCity, ip, city, urlOriginal: url, dateString }
-    
+
     const pageContext = await renderPage(pageContextInit);
 
     const { httpResponse } = pageContext
-    const { body, statusCode, headers} = httpResponse
+    const { body, statusCode, headers } = httpResponse
 
     return new Response(body, {
-        status:statusCode,
-        headers:headers
+        status: statusCode,
+        headers: headers
     })
 }
