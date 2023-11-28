419935806b78227cf512e30b962ac19d93d9949f
Dennoh <dennismwas89@gmail.com>
2023-11-28 23:22:14 +0300

    split to components

--- pages/components/Page.vue
+++ pages/components/Page.vue
@@ -22,33 +22,17 @@
             </div>
         </main>
         <div class="debug">Generated at {{ pageContext.dateString }}</div>
-        <footer>
-            <p class="company">
-                <a target="_blank" href="https://vercel.com" aria-label="Vercel">
-                    <LogoVercel />
-                </a>
-            </p>
-            <p class="details">
-                Built with
-                <a href="https://vuejs.org" target="_blank">Vue</a>
-                using
-                <a href="https://vite-plugin-ssr.com/" target="_blank">Vite SSR (Vike)</a>
-            </p>
-            <a target="_blank" href="https://github.com/dennismwas/vue-on-the-edge" class="source">
-                <LogoGithub />
-                Source
-            </a>
-        </footer>
+        <Footer />
     </div>
 </template>
 
 <script setup>
 import NavBar from './NavBar.vue';
 import Card from './Card.vue';
-import LogoVercel from './LogoVercel.vue';
-import LogoGithub from './LogoGithub.vue';
+import Footer from './Footer.vue'
 
 
-const props=defineProps(['greeting','pageContext'])
+
+const props = defineProps(['greeting', 'pageContext'])
 
 </script>
