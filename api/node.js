8b08968582045c56d30ace33a1c7ab8c1df02c13
Dennoh <dennismwas89@gmail.com>
2023-11-28 22:13:35 +0300

    added node runtime

--- api/node.js
+++ api/node.js
@@ -7,8 +7,8 @@ export default async function handler(request,response) {
 
 
     const dateString = new Date().toISOString();
-    const parsedCity = decodeURIComponent(request.headers.get('x-vercel-ip-city'));
-    const ip = (request.headers.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0];
+    const parsedCity = decodeURIComponent(request.headers['x-vercel-ip-city']);
+    const ip = (request.headers['x-forwarded-for'] ?? '127.0.0.1').split(',')[0];
     const city = parsedCity == 'undefined' ? "Cannot get city" : parsedCity;
 
     const pageContextInit = { parsedCity, ip, path:request.path, city, urlOriginal: url, dateString }
